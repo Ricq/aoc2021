4c7b076b47f94218962c627bf4dbca63e23c8bda
Rick Riemer <rick_riemer@hotmail.com>
2021-12-03 21:27:54 +0100

    Add Day 3

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,11 @@
 import day1.Day1
 import day2.Day2
+import day3.Day3
 
 import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
-    new Day2().runPart2();
+    new Day3().runPart2();
   }
 }
--- src/main/scala/day3/Day3.scala
+++ src/main/scala/day3/Day3.scala
@@ -2,22 +2,79 @@ package day3
 
 import util.Utils.using
 
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 class Day3 {
   def runPart1(): Unit = {
-
-    using(Source.fromFile("day3/test1.txt")) { file =>
-      for (line <- file.getLines()) {
+    def processLine(bitOccurrences: Array[Int])(line: String): Unit = {
+      bitOccurrences(0) = bitOccurrences(0) + 1
+      for ((ch, index) <- line.toList.zipWithIndex) {
+        if (ch == '1') {
+          bitOccurrences(index + 1) = bitOccurrences(index + 1) + 1
+        }
       }
     }
+
+    using(Source.fromFile("day3/input.txt")) { file =>
+      val lines = file.getLines()
+      val firstLine = lines.next()
+
+      val bitCount = firstLine.length
+      val bitOccurrences = Array.fill[Int](bitCount + 1)(0)
+
+      processLine(bitOccurrences)(firstLine)
+      lines.foreach(processLine(bitOccurrences))
+
+      val lineCount = bitOccurrences(0)
+      println(bitOccurrences.mkString(","))
+
+      val gammaBuilder = new StringBuilder
+      for (bit <- bitOccurrences.drop(1)) {
+        gammaBuilder.append(if (bit * 2 > lineCount) '1' else '0')
+      }
+      val gamma = Integer.parseInt(gammaBuilder.toString(), 2)
+      val epsilon = ~gamma & ((1 << bitCount) - 1)
+      println(s"Gamma rate: $gamma")
+      println(s"Epsilon rate: $epsilon")
+
+      val result = gamma * epsilon
+      println(s"Result: $result")
+    }
+
+
   }
 
 
   def runPart2(): Unit = {
-    using(Source.fromFile("day2/test1.txt")) { file =>
+    def reduce(numbers: List[Int], selector: (List[Int], List[Int]) => List[Int], bitMask: Int): Int = {
+      val (set, unset) = numbers.partition(number => (number & bitMask) > 0)
+      val result = selector(set, unset)
+      if (result.length == 1) result(0) else reduce(result, selector, bitMask >> 1)
+    }
+
+    val (input, bitCount) = readBinaryNumbers
+    val oxygenGeneratorRating = reduce(input, (set, unset) => if (set.length >= unset.length) set else unset, 1 << (bitCount - 1))
+    val co2ScrubberRating = reduce(input, (set, unset) => if (set.length < unset.length) set else unset, 1 << (bitCount - 1))
+    println(oxygenGeneratorRating)
+    println(co2ScrubberRating)
+    val lifeSupportRating = oxygenGeneratorRating * co2ScrubberRating
+    println(lifeSupportRating)
+  }
+
+  private def readBinaryNumbers: (List[Int], Int) = {
+    using(Source.fromFile("day3/input.txt")) { file =>
+      val input = new ListBuffer[Int]()
+      var bitCount = 0
       for (line <- file.getLines()) {
+        var number = 0;
+        for (ch <- line) {
+          number = (number << 1) | (if (ch == '1') 1 else 0)
+        }
+        input += number
+        bitCount = line.length
       }
+      (input.toList, bitCount)
     }
   }
 }
